369a67d4edd1dcdff62653ad12fe3a72f8dd1578
seoyeon <hongssup0202@gmail.com>
2021-12-23 16:40:43 +0900

    Initial Commit

---
+++ SpeechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechTest
+//
+//  Created by SeoYeon Hong on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
